083b0c82498cd8c25876d36d8764033e252eb992
KennethAlAr <kennethalonsoarce@gmail.com>
2025-10-15 02:20:02 +0200

    Clases e interfaz

--- src/main/java/biblioteca/simple/modelo/Producto.java
+++ src/main/java/biblioteca/simple/modelo/Producto.java
@@ -4,7 +4,47 @@ public abstract class Producto {
 
     protected int id;
     protected String titulo;
-    protected String ano;
+    protected String anho;
     protected Formato formato;
 
+    //Constructor para recibir objetos de la bbdd
+    protected Producto(int id, String titulo, String anho, Formato formato) {
+        this.id = id;
+        this.titulo = titulo;
+        this.anho = anho;
+        this.formato = formato;
+    }
+
+    //constructor para crear nuevos productos desde la aplicación
+    protected Producto(String titulo, String anho, Formato formato) {
+        this.titulo = titulo;
+        this.anho = anho;
+        this.formato = formato;
+    }
+
+    protected int getId() {
+        return id;
+    }
+
+    protected String getTitulo() {
+        return titulo;
+    }
+
+    protected String getAnho() {
+        return anho;
+    }
+
+    protected Formato getFormato() {
+        return formato;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", anho='" + anho + '\'' +
+                ", formato=" + formato +
+                '}';
+    }
 }
