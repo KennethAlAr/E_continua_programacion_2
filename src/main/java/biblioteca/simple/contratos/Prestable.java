5109ef2de67de70064939a4cfe6e260060d7d34c
KennethAlAr <kennethalonsoarce@gmail.com>
2025-11-09 14:40:06 +0100

    Base del trabajo de evaluación continua de programación

--- src/main/java/biblioteca/simple/contratos/Prestable.java
+++ src/main/java/biblioteca/simple/contratos/Prestable.java
@@ -2,10 +2,18 @@ package biblioteca.simple.contratos;
 
 import biblioteca.simple.modelo.Usuario;
 
+// La interfaz Prestable define el comportamiento que deben tener
+// los objetos que pueden ser prestados (como libros, películas, etc.)
 public interface Prestable {
 
-    void prestar (Usuario u);
-    void devolver();
-    boolean estaPrestado();
+    // Método para prestar el objeto a un usuario.
+    // Cada clase que implemente esta interfaz decidirá cómo gestionarlo internamente.
+    void prestar(Usuario u);
 
+    // Método para devolver el objeto prestado.
+    //void debe dejar el objeto como "no prestado".
+    void devolver();
+
+    // Método que indica si el objeto está actualmente prestado o no.
+    boolean estaPrestado();
 }
